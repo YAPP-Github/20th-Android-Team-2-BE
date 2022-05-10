d6829d088af3f95be5a57728a9784956af6dcce0
IceChoco <dkfk2685@naver.com>
2022-05-10 22:39:37 +0900

    feat: Setup best friend project

---
+++ src/main/java/yapp/bestFriend/BestFriendApplication.java
@@ -0,0 +1,13 @@
+package yapp.bestFriend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestFriendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestFriendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yapp/bestFriend/BestFriendApplicationTests.java
@@ -0,0 +1,13 @@
+package yapp.bestFriend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestFriendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
