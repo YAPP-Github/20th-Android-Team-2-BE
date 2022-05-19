5d78693ade148381bc820bdd7d6618d2193f7b40
김유비 <ieieie0419@gmail.com>
2022-05-20 00:59:04 +0900

    Feat 14: 절약을 등록하는 API 작성 ProductController ProductService CreateProductRequest (DTO) 구현

---
+++ src/main/java/yapp/bestFriend/service/ProductService.java
@@ -0,0 +1,30 @@
+package yapp.bestFriend.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import yapp.bestFriend.model.dto.DefaultRes;
+import yapp.bestFriend.model.dto.request.CreateProductRequest;
+import yapp.bestFriend.model.entity.User;
+import yapp.bestFriend.repository.ProductRepository;
+import yapp.bestFriend.repository.UserRepository;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+    private final UserRepository userRepository;
+
+    public DefaultRes createProduct(CreateProductRequest request) {
+        Optional<User> user = userRepository.findById(request.getUserId());
+
+        if(user.isPresent()){
+            productRepository.save(request.toEntity(user.get()));
+            return DefaultRes.response(HttpStatus.OK.value(), "등록 성공");
+        }
+        else return DefaultRes.response(HttpStatus.OK.value(), "등록 실패(사용자 정보 없음)");
+    }
+}
