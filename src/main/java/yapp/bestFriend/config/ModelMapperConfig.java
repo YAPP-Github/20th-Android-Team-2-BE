20f3471a1c7253995d75ac0ecf89adaacac0112c
IceChoco <dkfk2685@naver.com>
2022-06-01 13:00:57 +0900

    Chore: 모델매퍼 빈 추가

---
+++ src/main/java/yapp/bestFriend/config/ModelMapperConfig.java
@@ -0,0 +1,14 @@
+package yapp.bestFriend.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+}
