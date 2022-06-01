d4a35f0609982aa4f94a1b7eb5dcad79d36cb2e1
IceChoco <dkfk2685@naver.com>
2022-06-01 12:35:36 +0900

    Feat: 공통 칼럼 엔티티 이름 변경 및 삭제 여부 칼럼 추가

--- src/main/java/yapp/bestFriend/model/entity/BaseInfo.java
+++ src/main/java/yapp/bestFriend/model/entity/BaseInfo.java
@@ -1,6 +1,7 @@
 package yapp.bestFriend.model.entity;
 
 import lombok.Getter;
+import org.hibernate.annotations.Where;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
@@ -14,13 +15,14 @@ import java.time.LocalDateTime;
 
 
 /**
- * BaseTime은 모든 Entity의 상위 클래스가 되어 Entity들의 생성일시, 수정일시를 자동으로 관리하는 역할
+ * BaseInfo는 모든 Entity의 상위 클래스가 되어 Entity들의 생성일시, 수정일시를 자동으로 관리하는 역할
  * JPA Entity들이 @MappedSupperclass가 선언된 클래스를 상속할 경우 클래스의 필드들도 칼럼으로 인식하도록 한다.
  */
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public abstract class BaseTime{
+@Where(clause = "deleted=false")
+public abstract class BaseInfo {
 
     // Entity가 생성되어 저장될 때 시간이 자동 저장됩니다.
     @CreatedDate
@@ -36,5 +38,7 @@ public abstract class BaseTime{
 
     @LastModifiedBy
     private Long updatedBy;
+
+    private boolean deletedYn = Boolean.FALSE;
 
 }
