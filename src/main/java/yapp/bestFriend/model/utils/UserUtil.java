5ee727d65b0196d7088ddb088ec5909f4d77589b
IceChoco <dkfk2685@naver.com>
2022-06-01 12:58:58 +0900

    Feat #29: 로그인 사용자 조회를 위한 User Util 클래스 작성

---
+++ src/main/java/yapp/bestFriend/model/utils/UserUtil.java
@@ -0,0 +1,32 @@
+package yapp.bestFriend.model.utils;
+
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import yapp.bestFriend.model.entity.User;
+import yapp.bestFriend.service.user.UserDetails;
+
+public class UserUtil {
+
+    public static long getId() {
+        return getEntity().getId();
+    }
+
+    public static User getEntity() {
+        return getEntity(true);
+    }
+
+    public static User getEntity(boolean shouldHidePassword) {
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        User User = getDetails().getUser();
+        if (shouldHidePassword) {
+            User.setPassword(null);
+        }
+        return User;
+    }
+
+    public static UserDetails getDetails() {
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        return (UserDetails) securityContext.getAuthentication().getPrincipal();
+    }
+
+}
