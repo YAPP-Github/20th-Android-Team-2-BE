fc042e77c20de7daf9d9bbb22b0709a214b3b0fa
IceChoco <dkfk2685@naver.com>
2022-06-10 01:15:06 +0900

    FIX: 운영 DB, 테스트 DB 이원화로 인한 네이티브 쿼리 제거 및 JPQL로 변경

--- src/main/java/yapp/bestFriend/repository/SavingRecordRepository.java
+++ src/main/java/yapp/bestFriend/repository/SavingRecordRepository.java
@@ -13,30 +13,35 @@ import java.util.List;
 @Repository
 public interface SavingRecordRepository extends JpaRepository<SavingRecord, Long> {
 
-    // SQL 일반 파라미터 쿼리, @Param 사용 O
+    // JPQL 파라미터 쿼리, @Param 사용 O
+    // 22.06.10 수정 - 운영 DB, 테스트 DB 이원화로 인한 네이티브 쿼리 제거
     @Query(value =
-            "WITH SUMMMARY AS (\n" +
-            "    SELECT SUBSTR(A.RECORD_YMD,1,7) AS RECORD_MM,\n" +
-            "           B.PRODUCT_NAME,\n" +
-            "           SUM(DECODE(SUBSTR(A.RECORD_YMD,1,7),:toDate,1,0)) BASE_TIMES,\n" +
-            "           SUM(DECODE(SUBSTR(A.RECORD_YMD,1,7),:fromDate,1,0)) PREV_TIMES\n" +
-            "    From SAVING_RECORD A, PRODUCT B\n" +
-            "    WHERE A.PRODUCT_ID = B.PRODUCT_ID\n" +
-            "      AND A.USER_ID = B.USER_ID\n" +
-            "      AND A.USER_ID = :userId\n" +
-            "      AND A.RECORD_YMD BETWEEN PARSEDATETIME (:fromDate||'-01','yyyy-MM-dd') and PARSEDATETIME (:toDate||'-31','yyyy-MM-dd')\n" +
-            "    GROUP BY SUBSTR(A.RECORD_YMD,1,7), B.PRODUCT_NAME\n" +
-            ")\n" +
-            "SELECT RECORD_MM as recordMm, PRODUCT_NAME as productName, BASE_TIMES as baseTimes, PREV_TIMES as prevTimes, BASE_TIMES-PREV_TIMES AS timesComparedToPrev\n" +
-            "  FROM (\n" +
-            "    SELECT RECORD_MM, PRODUCT_NAME, CAST(NVL(SUM(BASE_TIMES),0) AS INT) AS BASE_TIMES,\n" +
-            "           CAST(NVL(SUM((SELECT PREV_TIMES FROM SUMMMARY WHERE RECORD_MM = :fromDate AND PRODUCT_NAME = A.PRODUCT_NAME)),0) AS INT) AS PREV_TIMES\n" +
-            "      FROM SUMMMARY A\n" +
-            "    WHERE RECORD_MM = :toDate\n" +
-            "    GROUP BY PRODUCT_NAME\n" +
-            ")",
-            nativeQuery = true)
-    List<SavingRecordSummaryInterface> selectSummary(@Param(value = "fromDate") String fromDate, @Param(value = "toDate") String toDate, @Param(value = "userId") Long userId);
+            "select substring(A.recordYmd,1,7) AS recordMm,\n" +
+                    "       A.product.name AS productName\n" +
+                    "       ,SUM(case substring(A.recordYmd,1,7)\n" +
+                    "             when :toDate \n" +
+                    "             then 1 \n" +
+                    "             else 0 \n" +
+                    "         end) AS baseTimes\n" +
+                    "       ,SUM(case substring(A.recordYmd,1,7)\n" +
+                    "             when :fromDate \n" +
+                    "             then 1 \n" +
+                    "             else 0 \n" +
+                    "         end) AS prevTimes\n" +
+                    " from SavingRecord A inner join A.product B\n" +
+                    "WHERE A.user.id = :userId\n" +
+                    "  AND A.recordYmd BETWEEN :from AND :to\n" +
+                    "  AND A.deletedYn = false\n" +
+                    "  AND B.deletedYn = false\n" +
+                    "GROUP BY substring(A.recordYmd,1,7), A.product.name"
+            )
+    List<SavingRecordSummaryInterface> selectSummary(@Param(value = "fromDate") String fromDate,
+                                                     @Param(value = "toDate") String toDate,
+                                                     @Param(value = "from") LocalDate from,
+                                                     @Param(value = "to") LocalDate to,
+                                                     @Param(value = "userId") Long userId);
 
-    List<SavingRecord> findSavingRecordsByProductIdAndUserIdAndRecordYmdAndDeletedYn(LocalDate recordYmd, Long productId, Long userId, Boolean DeletedYn); // where name = ? and ranking = ?
+    List<SavingRecord> findSavingRecordsByRecordYmdAndProductIdAndUserIdAndDeletedYn(LocalDate recordYmd, Long productId, Long userId, Boolean DeletedYn); // where name = ? and ranking = ?
+
+
 }
