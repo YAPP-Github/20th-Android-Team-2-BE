9af7eec291ce5786c48378181942b8d796dce17f
IceChoco <dkfk2685@naver.com>
2022-06-13 15:39:56 +0900

    FIX: java 실행 파일 구문 수정

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-REPOSITORY=/home/ec2-user/app/step1
+REPOSITORY=/home/ec2-user/app/step2
 PROJECT_NAME=20th-Android-Team-2-BE
 APP_NAME=bestFriend
 
 echo "> Build 파일 복사"
 
-cp $REPOSITORY/$PROJECT_NAME/build/libs/*.jar $REPOSITORY/
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl ${APP_NAME} | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl ${APP_NAME} | grep java | awk '{print $1}')
 
 echo "> 현재 구동중인 애플리케이션 pid: $CURRENT_PID"
 
@@ -24,7 +24,7 @@ fi
 
 echo "> 새 어플리케이션 배포"
 
-JAR_NAME=$(ls -tr $REPOSITORY/ | grep jar | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 
@@ -36,7 +36,7 @@ echo "> $JAR_NAME 실행"
 
 nohup java -jar \
     -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-prod.properties \
-    "-Dspring.profiles.active=prod" $REPOSITORY/$JAR_NAME 2>&1 & \
+    "-Dspring.profiles.active=prod" \
     $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
 
 
