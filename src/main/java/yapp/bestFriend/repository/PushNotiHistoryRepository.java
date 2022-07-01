63bd1e850b176289a89e7f28d460ac78d8c88547
IceChoco <dkfk2685@naver.com>
2022-07-01 13:05:17 +0900

    Feature: GET api/alarm 구현 및 테스트 코드 작성

--- src/main/java/yapp/bestFriend/repository/PushNotiHistoryRepository.java
+++ src/main/java/yapp/bestFriend/repository/PushNotiHistoryRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import yapp.bestFriend.model.entity.PushNotiHistory;
 
+import java.util.List;
+
 @Repository
 public interface PushNotiHistoryRepository extends JpaRepository<PushNotiHistory, Long> {
+    List<PushNotiHistory> findByUserIdAndDeletedYn(long userId, Boolean DeletedYn); // where name = ? and ranking = ?
 }
