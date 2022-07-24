86506f0ebfa36aa466d1c7c66ac1a9fbb2e1da86
IceChoco <dkfk2685@naver.com>
2022-07-24 23:25:43 +0900

    Feat: email, 비밀번호로 유저정보를 찾는 메서드 추가

--- src/main/java/yapp/bestFriend/repository/UserRepository.java
+++ src/main/java/yapp/bestFriend/repository/UserRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import yapp.bestFriend.model.entity.User;
-import yapp.bestFriend.model.entity.UserConnection;
 import yapp.bestFriend.model.enumClass.SocialLoginType;
 
 import java.util.Optional;
@@ -15,8 +14,14 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByEmail(String email);
 
+    @Query("SELECT user.email FROM User user WHERE user.email = :email")
+    Optional<String> findFirstByEmail(String email);
+
     @Query(value = "SELECT A FROM User A WHERE A.id = :userId and A.deletedYn = false")
     Optional<User> findById(@Param(value = "userId") Long id);
 
     User findByEmailAndProviderAndProviderId(String email, SocialLoginType provider, String providerId);
+
+    @Query("SELECT user FROM User user WHERE user.email = :email and user.password =:password")
+    Optional<User> findByUser(@Param("email") String email, @Param("password") String password);
 }
