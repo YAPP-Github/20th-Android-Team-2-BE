16c80e9d2187a6be5273ee1f436123c1ecafd536
IceChoco <dkfk2685@naver.com>
2022-07-24 23:26:30 +0900

    Test: 자체 회원가입, 자체 로그인 api 개발에 따른 테스트 코드 5개 신규 작성

--- src/test/java/yapp/bestFriend/service/user/UserServiceTest.java
+++ src/test/java/yapp/bestFriend/service/user/UserServiceTest.java
@@ -5,12 +5,15 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockedStatic;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import yapp.bestFriend.model.dto.request.user.UserSignUpRequestDto;
 import yapp.bestFriend.model.entity.Role;
 import yapp.bestFriend.model.entity.User;
 import yapp.bestFriend.model.entity.UserConnection;
 import yapp.bestFriend.model.enumClass.SocialLoginType;
+import yapp.bestFriend.model.utils.JwtUtil;
 import yapp.bestFriend.repository.UserConnectionRepository;
 import yapp.bestFriend.repository.UserFcmTokenRepository;
 import yapp.bestFriend.repository.UserRepository;
@@ -19,6 +22,7 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
@@ -79,5 +83,96 @@ class UserServiceTest {
         assertThat(userService.logout(1L))
                 .hasFieldOrPropertyWithValue("statusCode", HttpStatus.NOT_FOUND.value())
                 .hasFieldOrPropertyWithValue("message", "사용자 정보가 존재하지 않습니다.");
+    }
+
+    @Test
+    @DisplayName("이미 있는 이메일인경우 회원가입 불가")
+    void login_email_duplicate() {
+        //when
+        when(userRepository.findFirstByEmail(any())).thenReturn(Optional.of("test@naver.com"));
+
+        UserSignUpRequestDto userSignUpRequestDto =
+                new UserSignUpRequestDto("test@naver.com","MTIzNDU2");
+
+        //then
+        assertThat(userService.signUp(userSignUpRequestDto))
+                .hasFieldOrPropertyWithValue("statusCode", HttpStatus.OK.value())
+                .hasFieldOrPropertyWithValue("message", "이메일중복");
+    }
+
+    @Test
+    @DisplayName("자체회원가입 정상")
+    void login_success_normally() {
+        //when
+        when(userRepository.findFirstByEmail(any())).thenReturn(Optional.empty());
+
+        UserSignUpRequestDto userSignUpRequestDto =
+                new UserSignUpRequestDto("test@naver.com","MTIzNDU2");
+
+        //then
+        assertThat(userService.signUp(userSignUpRequestDto))
+                .hasFieldOrPropertyWithValue("statusCode", HttpStatus.OK.value())
+                .hasFieldOrPropertyWithValue("message", "성공");
+    }
+
+    @Test
+    @DisplayName("자체 로그인 정상")
+    void signIn_normally() {
+        //given
+        User user = User.builder()
+                .email("test@naver.com")
+                .password("123456")
+                .nickName("best friend")
+                .role(Role.USER)
+                .userConnection(null)
+                .build();
+
+        //when
+        when(userRepository.findFirstByEmail(any())).thenReturn(Optional.of("test@naver.com"));
+        when(userRepository.findByUser(any(), any())).thenReturn(Optional.of(user));
+
+        UserSignUpRequestDto userSignUpRequestDto =
+                new UserSignUpRequestDto("test@naver.com","MTIzNDU2");
+
+        try (MockedStatic<JwtUtil> mockedStatic = mockStatic(JwtUtil.class)) {
+            mockedStatic.when(() -> JwtUtil.createAccessToken(any())).thenReturn("accessToken");
+            mockedStatic.when(() -> JwtUtil.createRefreshToken(any())).thenReturn("refreshToken");
+
+            //then
+            assertThat(userService.signIn(userSignUpRequestDto))
+                    .hasFieldOrPropertyWithValue("statusCode", HttpStatus.OK.value())
+                    .hasFieldOrPropertyWithValue("message", "등록성공");
+        }
+    }
+
+    @Test
+    @DisplayName("자체 로그인 비정상 - 비밀번호 불일치")
+    void signIn_wrong_password() {
+        //when
+        when(userRepository.findFirstByEmail(any())).thenReturn(Optional.of("test@naver.com"));
+        when(userRepository.findByUser(any(), any())).thenReturn(Optional.empty());
+
+        UserSignUpRequestDto userSignUpRequestDto =
+                new UserSignUpRequestDto("test@naver.com","MTIzNDU2");
+
+        //then
+        assertThat(userService.signIn(userSignUpRequestDto))
+                .hasFieldOrPropertyWithValue("statusCode", HttpStatus.OK.value())
+                .hasFieldOrPropertyWithValue("message", "비밀번호불일치");
+    }
+
+    @Test
+    @DisplayName("자체 로그인 비정상 - 아이디 불일치")
+    void signIn_wrong_Id() {
+        //when
+        when(userRepository.findFirstByEmail(any())).thenReturn(Optional.empty());
+
+        UserSignUpRequestDto userSignUpRequestDto =
+                new UserSignUpRequestDto("test@naver.com","MTIzNDU2");
+
+        //then
+        assertThat(userService.signIn(userSignUpRequestDto))
+                .hasFieldOrPropertyWithValue("statusCode", HttpStatus.OK.value())
+                .hasFieldOrPropertyWithValue("message", "아이디불일치");
     }
 }
